91016ab0f518b9d4cecbc500813ac894805d6b51
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2024-04-13 23:27:16 +0530

    CSS to cards and used data binding concept

--- src/components/ViewBus.jsx
+++ src/components/ViewBus.jsx
@@ -1,87 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
+import '../App.css'
 
 const ViewBus = () => {
+    const [ksrtc, changeKsrtc] = useState(
+        [
+            { "name": "KSRTC 1", "from": "Alappuzha", "to": "Kochi", "type": "Super Fast", "fare": "Rs. 200", "img": "https://wallpapercave.com/wp/wp8970009.jpg" },
+            { "name": "KSRTC 2", "from": "Kottarakkara", "to": "Coimbature", "type": "Super Fast", "fare": "Rs. 300", "img": "https://wallpapercave.com/wp/wp7570214.jpg" },
+            { "name": "KSRTC 3", "from": "Ernakulam", "to": "Kottayam", "type": "Super Fast", "fare": "Rs. 100", "img": "https://wallpapercave.com/wp/wp8970105.jpg" },
+            { "name": "KSRTC 4", "from": "Palakkad", "to": "Kochi", "type": "Super Fast", "fare": "Rs. 400", "img": "https://wallpapercave.com/wp/wp8970114.jpg" },
+            { "name": "KSRTC 5", "from": "Theni", "to": "Kochi", "type": "Super Fast", "fare": "Rs. 500", "img": "https://wallpapercave.com/wp/wp8970137.jpg" }
+
+        ]
+    )
+
     return (
+
         <div>
-            <Navbar/>
+            <Navbar />
 
             <div className="container">
                 <div className="row">
-                    <col className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" />
-                    <br /><br />
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                    <div className="row g-5">
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <br /><br />
+                        <div className="row g-3">
+                        {
+                        ksrtc.map(
+                            (value)=>{
+
+                                return <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <div class="card" width="10rem">
-                                <img src="https://wallpapercave.com/wp/wp8970009.jpg" class="card-img-top" alt="..." />
+                                <img src={value.img} class="card-img-top" alt="..." />
                                 <div class="card-body">
-                                    <h5 class="card-title">KSRTC 1</h5>
-                                    <p class="card-text">Alappuzha KSRTC</p>
+                                    <h5 class="card-title">{value.name}</h5>
+                                    <p class="card-text">{value.from} to {value.to}</p>
+                                    <p class="card-text">Type : {value.type}</p>
+                                    <p class="card-text">Fare : {value.fare}</p>
                                     <a href="#" class="btn btn-secondary">View Details</a>
                                 </div>
                             </div>
 
 
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <div class="card">
-                                <img src="https://wallpapercave.com/wp/wp7570214.jpg" class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">KSRTC 2</h5>
-                                    <p class="card-text">Kottarakkara - Coimbature KSRTC</p>
-                                    <a href="#" class="btn btn-secondary">View Details</a>
-                                </div>
-                            </div>
 
-
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <div class="card">
-                                <img src="https://wallpapercave.com/wp/wp8970105.jpg" class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">KSRTC 3</h5>
-                                    <p class="card-text">Ernakulam KSRTC</p>
-                                    <a href="#" class="btn btn-secondary">View Details</a>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <div class="card">
-                                <img src="https://wallpapercave.com/wp/wp8970114.jpg" class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">KSRTC 4</h5>
-                                    <p class="card-text">Palakkad KSRTC</p>
-                                    <a href="#" class="btn btn-secondary">View Details</a>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <div class="card">
-                                <img src="https://wallpapercave.com/wp/wp8970137.jpg" class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">KSRTC 5</h5>
-                                    <p class="card-text">Theni KSRTC</p>
-                                    <a href="#" class="btn btn-secondary">View Details</a>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
+                            }
+                        )
+                       }
+                            
                         </div>
                     </div>
                 </div>
             </div>
+
+
+
         </div>
     )
 }
